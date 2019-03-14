d155eacaee4d6cd57d026119dd22231468d7997c
MIGLIAVACCA Diego <diego.migliavacca@ext.ec.europa.eu>
2019-03-14 15:42:32 +0100

    fixed gradle build

---
+++ platforms/android/app/build/generated/source/buildConfig/debug/eu/jrc/treechecker/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package eu.jrc.treechecker;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "eu.jrc.treechecker";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10000;
+  public static final String VERSION_NAME = "1.0.0";
+}
