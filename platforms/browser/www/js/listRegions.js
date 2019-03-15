25a2bb70264566f2d7f33ff2f659dc68c4c07330
MIGLIAVACCA Diego <diego.migliavacca@ext.ec.europa.eu>
2019-03-15 17:19:14 +0100

    added list regions function

--- platforms/browser/www/js/listRegions.js
+++ platforms/browser/www/js/listRegions.js
@@ -12,19 +12,17 @@ var listRegions = {
         window.plugins.spinnerDialog.show();
         var token = window.sessionStorage.getItem("token");
 
-        /*$.ajax({
-            type : 'GET',
-            crossDomain : true,
-            url : 'http://127.0.0.1:8001/api/gzs/',
-            beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'JWT ' + token);},
-            success : function(reg) {
-                var html = "";
-                $.each(reg, function(key, val) {
-                    html += '<div class="card"><img id="img-card" class="card-img-top" src="img/' + val.image_url + '" alt="Image"><div class="card-body"><h5 class="card-title">' + val.name + '</h5><a href="map.html" class="btn button">Go</a></div></div>';
-                });
-                $("#listregions-page").html(html);
-                window.plugins.spinnerDialog.hide();
-            },
+        listRegions = function (tx, res) {
+            var html = "";
+            $.each(res, function(key, val) {
+                html += '<div class="card"><img id="img-card" class="card-img-top" src="img/' + val.image_url + '" alt="Image"><div class="card-body"><h5 class="card-title">' + val.name + '</h5><a href="map.html" class="btn button">Go</a></div></div>';
+            });
+            $("#listregions-page").html(html);
+            window.plugins.spinnerDialog.hide();
+        }
+        getRegions(listRegions);
+
+        /*
             error : function(req, status, error) {
                 window.plugins.spinnerDialog.hide();
                 if(document.getElementById("errorpopupdata").getElementsByTagName('p').length <= 0){
@@ -50,7 +48,11 @@ var listRegions = {
                     $.each(reg, function(key, val) {
                         db.transaction(function(tx) {
                             tx.executeSql("REPLACE INTO geographicalzone(id, name, layer_name, wms_url, proj, image_url, x_min, x_max, y_min, y_max) VALUES(" + val.id + "," + val.name + "," + val.layer_name + "," + val.wms_url + "," + val.proj + "," + val.image_url + "," + val.x_min + "," + val.x_max + "," + val.y_min + "," + val.y_max + ")");
-                        });
+                        }, function(error) {
+                             console.log('Transaction ERROR: ' + error.message);
+                           }, function() {
+                             console.log('Populated database OK');
+                           });
                     });
                     window.plugins.spinnerDialog.hide();
                 },
--- www/js/db.js
+++ www/js/db.js
@@ -4,3 +4,9 @@ var db;
 function openDb() {
     db = window.sqlitePlugin.openDatabase({name:'treechecker.db', location:'default'});
 };
+
+getRegions = function(fn) {
+    db.transaction(function(tx) {
+        tx.executeSql('SELECT * FROM geographicalzone, [], fn);
+    });
+};
