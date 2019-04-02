30f5a5830e961986dea0aeadccbc7d20056b4118
PL <pierric.delaborie@gmail.com>
2019-04-02 19:24:30 +0200

    corrected code

--- www/js/listRegions.js
+++ www/js/listRegions.js
@@ -16,9 +16,9 @@ var listRegions = {
                 var query = 'SELECT * FROM geographicalzone';
                 tx.executeSql(query, [], function (tx, res) {
                     var html = "";
-                    $.each(res, function(key, val) {
-                        html += '<div class="card"><img id="img-card" class="card-img-top" src="img/' + val.image_url + '" alt="Image"><div class="card-body"><h5 class="card-title">' + val.name + '</h5><a href="map.html" class="btn button">Go</a></div></div>';
-                    });
+                    for(var x = 0; x < res.rows.length; x++) {
+                        html += '<div class="card"><img id="img-card" class="card-img-top" src="img/' + res.rows.item(x).image_url + '" alt="Image"><div class="card-body"><h5 class="card-title">' + res.rows.item(x).name + '</h5><a href="map.html" class="btn button">Go</a></div></div>';
+                    }
                     $("#listregions-page").html(html);
                     window.plugins.spinnerDialog.hide();
                 },
@@ -30,15 +30,6 @@ var listRegions = {
             }, function () {
                 console.log('transaction ok');
             });
-        /*
-            error : function(req, status, error) {
-                window.plugins.spinnerDialog.hide();
-                if(document.getElementById("errorpopupdata").getElementsByTagName('p').length <= 0){
-                    $("#errorpopupdata").prepend("<p>Error - No connection to the DB.</p>");
-                }
-                $("#errorpopupdata").popup("open", {transition:"fade",positionTo:"window"});
-            }
-        })*/
 
         $('#sidebarCollapse').on('click', function() {
             $('#sidebar').toggleClass('active');
