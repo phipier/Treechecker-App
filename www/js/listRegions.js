77bac90206c5b22a5c4b0c61aac1e25176a0c08d
PL <pierric.delaborie@gmail.com>
2019-04-03 19:30:21 +0200

    corrected sql syntax

--- www/js/listRegions.js
+++ www/js/listRegions.js
@@ -46,7 +46,12 @@ var listRegions = {
                 success : function(reg) {
                     $.each(reg, function(key, val) {
                         db.transaction(function(tx) {
-                            tx.executeSql("REPLACE INTO geographicalzone(id, name, layer_name, wms_url, proj, image_url, x_min, x_max, y_min, y_max) VALUES(" + val.id + "," + val.name + "," + val.layer_name + "," + val.wms_url + "," + val.proj + "," + val.image_url + "," + val.x_min + "," + val.x_max + "," + val.y_min + "," + val.y_max + ")");
+                            var sqlstr = "REPLACE INTO geographicalzone("
+                            + "id, name, layer_name, wms_url, proj, image_url, x_min, x_max, y_min, y_max) "
+                            + "VALUES("+val.key+",'"+val.name+"','layer_name','wms_url','proj'"
+                            + ",'"+val.image_url+"',"+val.bbox[0]+","+val.bbox[1]+","+val.bbox[2]+","+val.bbox[3]+")";
+                            
+                            tx.executeSql(sqlstr);                            
                         }, function(error) {
                              console.log('Transaction ERROR: ' + error.message);
                            }, function() {
--- www/js/urls.js
+++ www/js/urls.js
@@ -1,2 +1,2 @@
-URLSERVER = 'http://10.0.2.2:8001';
+URLSERVER = 'http://10.0.2.2:8000';
 //URLSERVER : 'http://127.0.0.1:8001',
