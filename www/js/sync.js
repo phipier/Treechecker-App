b7c423acc948d267eef7b1b3f2004fc59aaca45c
PL <pierric.delaborie@gmail.com>
2019-04-04 18:32:30 +0200

    drafted update and sync actions

--- www/js/sync.js
+++ www/js/sync.js
@@ -47,34 +47,25 @@ function update_crowndiameter() {}
 function update_treespecies() {}
 
 function sync_AOIandOBS() {
-    $.ajax({
-        type : 'GET',
-        crossDomain : true,
-        url : URLSERVER + '/api/gzs/',
-        beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'JWT ' + token);},
-        success : function(reg) {
-            $.each(reg, function(key, val) {
-                db.transaction(function(tx) {
-                    
-                    var sqlstr = "REPLACE INTO geographicalzone("
-                    + "id, name, layer_name, wms_url, proj, image_url, x_min, x_max, y_min, y_max) "
-                    + "VALUES("+val.key+",'"+val.name+"','layer_name','wms_url','proj'"+",'"+val.image_url
-                    + "',"+val.bbox[0]+","+val.bbox[1]+","+val.bbox[2]+","+val.bbox[3]+")";
+// AOI
+   // LOCAL actions
+   // Delete in local DB all AOI marked as "deleted" in server DB (+ delete from server DB)
+   // insert records from ServerDB where local id does not exists
 
-                    tx.executeSql(sqlstr);                            
-                }, function(error) {
-                    console.log('Transaction ERROR: ' + error.message);
-                }, function() {
-                    console.log('Populated database OK');
-                });
-            });
-            window.plugins.spinnerDialog.hide();
-        },
-        error : function(req, status, error) {
-            window.plugins.spinnerDialog.hide();
-            console.log("no connection to DB");
-        }
-    });
+   // SERVER actions
+   // Delete in server DB all AOI marked as "deleted" in local DB (+ delete from local DB)
+   // insert records from localDB where local id does not exists 
+
+// Survey observations
+   // LOCAL actions
+   // delete OBS in local DB marked as "deleted" in server DB (+ delete from server DB) 
+   // replace (update) all records in local DB from server DB where id_local = id_server AND (server DB update date > locate DB update date)
+   // insert records from ServerDB where local id does not exists
+   
+   // SERVER actions
+   // delete in server DB all OBS marked as "deleted" in local DB (+ delete from local DB)
+   // replace (update) OBS to server DB where id_local = id_server AND (server DB update date < locate DB update date)
+   // insert records from ServerDB where local id does not exists
 }
 
 function sync_Images() {}
