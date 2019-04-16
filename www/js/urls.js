537aed158c8db5c96669133744d3e8ad0423e822
phipier <diego.migliavacca@ext.ec.europa.eu>
2019-04-16 14:31:35 +0200

    fixed browser url

--- www/js/urls.js
+++ www/js/urls.js
@@ -1,12 +1,11 @@
 const SERVERURL = 'http://10.0.2.2:8000';
-//const SERVERURL : 'http://127.0.0.1:8001',
+//const SERVERURL = 'http://127.0.0.1:8001';
 //const SERVERURL = 'https://treechecker.ies.jrc.it';
 //const SERVERURL = 'http://0e097f6f.gclientes.com';
 //const SERVERURL = 'http://192.168.43.148:8000';
 //const SERVERURL = 'http://10.191.240.1:8000';
 
 const LayerDefinitions = {
-
     osm: {
             layerName: 'OSM',
             // url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -34,5 +33,4 @@ const LayerDefinitions = {
             crs: 'EPSG:4326',
     },
     downloadables: ['jrcOrthophotosWMS','jrcGeometriesWMS']
-
 }
