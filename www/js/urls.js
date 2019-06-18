69c5b446c078a00f3fff0c7867d36ba4ed7f901a
PL <pierric.delaborie@gmail.com>
2019-06-18 22:59:55 +0200

    url correction

--- www/js/urls.js
+++ www/js/urls.js
@@ -1,5 +1,4 @@
 const SERVERURL = 'http://10.0.2.2:8000';
-const SERVERURL = 'https://treechecker.pythonanywhere.com';
 //const SERVERURL = 'http://127.0.0.1:8001';
 //const SERVERURL = 'https://treechecker.ies.jrc.it';
 //const SERVERURL = 'http://0e097f6f.gclientes.com';
