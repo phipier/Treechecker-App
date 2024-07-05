d4537acaf2c248af17b85e481c08971d97a3a055
Pierric <pierric.delaborie@gmail.com>
2024-07-05 16:24:38 +0200

    Update login.js

--- www/js/login.js
+++ www/js/login.js
@@ -1,13 +1,8 @@
 var email;
 var password;
 
-//const SERVERURL = 'http://10.0.2.2:8000';
 const SERVERURL = 'https://treechecker.eu.pythonanywhere.com';
-//const SERVERURL = 'http://192.168.43.148:8000';
-//const SERVERURL = 'https://treechecker.ies.jrc.it';
-
 //const SERVERURL = 'http://127.0.0.1:8001';
-//const SERVERURL = 'http://10.191.240.1:8000';
 
 var login = {
 
